e71811bb994871902779624dbfeebb9404d703b2
j415301 <76389633+j415301@users.noreply.github.com>
2021-01-20 02:08:15 +0900

    Add files via upload

--- 05_array/src/com/array/run/Main.java
+++ 05_array/src/com/array/run/Main.java
@@ -1,10 +1,17 @@
 package com.array.run;
+
 import com.array.controller.ArrayController;
+import com.array.controller.DoubleArray;
 
 public class Main {
 	public static void main(String[] args) {
 		
-		new ArrayController().basicArray();
+		//new ArrayController().basicArray();
+		//new ArrayController().changeData();
+		//new ArrayController().practice1();
+		//new ArrayController().randomTest();
+		//new ArrayController().copyArray();
+		new DoubleArray().basicDoubleArray();
 		
 	}
 
