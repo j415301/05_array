f675cb438097d77c9def44c16cb3b8a069b8e5ac
j415301 <76389633+j415301@users.noreply.github.com>
2021-01-20 17:02:01 +0900

    Add files via upload

--- 05_array/src/com/array/run/MainPractice.java
+++ 05_array/src/com/array/run/MainPractice.java
@@ -1,11 +1,18 @@
 package com.array.run;
+
 import com.array.controller.ArraycontrollerPractice;
+import com.array.controller.DoubleArrayPractice;
 
 public class MainPractice {
 	
 	public static void main(String[] args) {
 		
-		new ArraycontrollerPractice().basicArray();
+		//new ArraycontrollerPractice().basicArray();
+		//new ArraycontrollerPractice().changeData();
+		//new ArraycontrollerPractice().practice1();
+		//new ArraycontrollerPractice().randomTest();
+		//new ArraycontrollerPractice().copyArray();
+		new DoubleArrayPractice().basicDoubleArray();
 		
 	}
 
